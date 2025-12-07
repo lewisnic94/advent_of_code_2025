99f3518db7a92fad72ca163faad31a8e19a02fd2
lewisnic94 <lewis@flowertech.se>
2025-12-07 19:27:17 +0100

    day 7

---
+++ d7/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	dat, _ := os.ReadFile("beams.input")
+	rows := strings.Split(string(dat), "\n")
+	incomingBeams := map[int]int{strings.Index(rows[0], "S"): 1}
+	splitCount, totalTimelines := 0, 0
+	for _, row := range rows {
+		splitterLocations := map[int]bool{}
+		for c, ch := range []rune(row) {
+			if ch == '^' {
+				splitterLocations[c] = true
+			}
+		}
+		outgoingBeams := map[int]int{}
+		for idx, count := range incomingBeams {
+			if splitterLocations[idx] {
+				outgoingBeams[idx-1] += count
+				outgoingBeams[idx+1] += count
+				splitCount++
+			} else {
+				outgoingBeams[idx] += count
+			}
+		}
+		incomingBeams = outgoingBeams
+	}
+	for _, count := range incomingBeams {
+		totalTimelines += count
+	}
+	fmt.Println("number of timelines:", totalTimelines, "number of splits:", splitCount)
+}
